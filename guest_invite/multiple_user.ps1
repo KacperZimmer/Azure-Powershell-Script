bd127988c32f03b30c4b12cb7fc534fc123bb3c9
Kacper <kacperzimmer@outlook.com>
2024-11-25 14:30:04 +0100

    don

---
+++ guest_invite/multiple_user.ps1
@@ -0,0 +1,44 @@
+
+Import-Module Microsoft.Graph
+
+
+Connect-MgGraph -Scopes "User.Invite.All"
+
+
+function Send-MgInvitations {
+    param (
+        [Parameter(Mandatory = $true)]
+        [array]$UserList, 
+        [Parameter(Mandatory = $true)]
+        [string]$RedirectUrl 
+    )
+    foreach ($User in $UserList) {
+
+        $messageInfo = @{
+            MessageLanguage = "en-US"
+            CustomizedMessageBody = "You have been invited to access the application. Click the link to accept the invitation."
+        }
+
+        try {
+
+            $invitation = New-MgInvitation `
+                -InvitedUserEmailAddress $User.Email `
+                -InvitedUserDisplayName $User.Name `
+                -InviteRedirectUrl $RedirectUrl `
+                -InvitedUserMessageInfo $messageInfo `
+                -SendInvitationMessage: $true
+
+            Write-Host "Zaproszenie wysłane do: $($User.Email) (ID zaproszenia: $($invitation.Id))" -ForegroundColor Green
+        } catch {
+            Write-Host "Błąd przy wysyłaniu zaproszenia do: $($User.Email). Szczegóły: $_" -ForegroundColor Red
+        }
+    }
+}
+
+$usersToInvite = @(
+    @{ Email = "user1@example.com"; Name = "User One" },
+    @{ Email = "user2@example.com"; Name = "User Two" },
+    @{ Email = "user3@example.com"; Name = "User Three" }
+)
+
+Send-MgInvitations -UserList $usersToInvite -RedirectUrl "https://myapplications.microsoft.com/?tenantid=aaaabbbb-0000-cccc-1111-dddd2222eeee"
